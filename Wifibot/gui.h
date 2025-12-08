28f4391f3549c253b8d2a4e1a7b43a057f588bdb
juliegbblz <zilberbergjulie@gmail.com>
2025-12-08 14:12:41 +0100

    Commit initial

---
+++ Wifibot/gui.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "wifibot.h"
+#include <gtkmm/window.h>
+#include <gtkmm/button.h>
+#include <gtkmm/frame.h>
+#include <gtkmm/box.h>
+#include <gtkmm/entry.h>
+#include <gtkmm/table.h>
+#include <gtkmm/label.h>
+#include <glibmm.h>      
+#include <sigc++/sigc++.h> 
+
+#include <iostream>
+
+
+
+
+
+class Gui : public Gtk::Window 
+{
+private:
+	Wifibot m_robot;
+	Gtk::Button m_bouton_1;
+	Gtk::Button m_bouton_up;
+	Gtk::Button m_bouton_down;
+	Gtk::Button m_bouton_stop;
+	Gtk::Button m_bouton_left;
+	Gtk::Button m_bouton_right;
+	Gtk::Button m_bouton_rot_l;
+	Gtk::Button m_bouton_rot_r;
+	Gtk::Entry m_entree_1;
+	Gtk::Entry m_entree_batterie;
+	Gtk::Label titre;
+	Gtk::Label fin;
+	
+
+public:
+	Gui();
+	~Gui();
+	bool time_out();
+
+
+};
---
+++ Wifibot/wifibot.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <thread>
+#include "order.h"
+#include "socket.h"
+
+#define LOOP_TIME 200
+#define PORT 15020
+
+class Wifibot {
+private:
+	Order m_order;
+	std::thread* m_p_thread;
+	std::thread* m_p_thread_recv;
+	bool m_stop;
+	Socket_TCP m_socket;
+	char m_output_buf[9];
+	unsigned char trame_crc[6];
+	char data_robot[21];
+	
+
+public:
+	Wifibot();
+	~Wifibot();
+	void stop();
+	void speed_up();
+	void speed_down();
+	void turn(int direction);
+	void rotate(int direction);
+	void connect(std::string ip);
+	void disconnect();
+	int getBattery();
+	
+
+
+private:	
+	void run();
+	void receive();
+	short crc16(unsigned char*,int);
+
+};
